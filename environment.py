ca1168b01fea13221da9db63aa6642d65e10ad12
leon <74154193+lz381@users.noreply.github.com>
2020-12-04 10:42:22 +0000

    Added minor comments for clarity.

--- environment.py
+++ environment.py
@@ -11,14 +11,17 @@ class ENVIRONMENT:
         
     def Send_To(self, sim):
         
+        # first shot at goal
         if self.ID == 0:
             ball = sim.send_sphere(x=0, y=6, z=self.ball_z, radius=self.ball_radius, collision_group = 'ball')
             sim.send_external_force(ball, x=0, y=-10, z=0, time=5)
-            
+        
+        # second shot at goal    
         if self.ID == 1:
             ball = sim.send_sphere(x=2, y=6, z=self.ball_z, radius=self.ball_radius, collision_group = 'ball')
             sim.send_external_force(ball, x=0, y=-10, z=0, time=5)
-            
+        
+        # third shot at goal
         if self.ID == 2:
             ball = sim.send_sphere(x=-2, y=6, z=self.ball_z, radius=self.ball_radius, collision_group = 'ball')
             sim.send_external_force(ball, x=0, y=-10, z=0, time=5)
