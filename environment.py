946e16437d5bebf30f8e9244e71e261863476c06
leon <74154193+lz381@users.noreply.github.com>
2020-12-11 13:30:25 +0000

    fixed goalpost param bug

--- environment.py
+++ environment.py
@@ -13,12 +13,12 @@ class ENVIRONMENT:
         
         # goalpost params
         goal_width = 5
-        goal_height = 1.8
+        goal_height = 2
         
         # goalpost object
-        left_post = sim.send_cylinder(x=goal_width/2, y=0, z=0.9, length=goal_height, radius=0.05, collision_group = 'goalpost', mass=99)
-        right_post = sim.send_cylinder(x=-goal_width/2, y=0, z=0.9, length=goal_height, radius=0.05, collision_group = 'goalpost', mass=99)
-        crossbar = sim.send_cylinder(x=0, y=0, z=goal_height, r1=1, r2=0, r3=0, length=goal_width, radius=0.05, collision_group = 'goalpost', mass=99)
+        left_post = sim.send_cylinder(x=goal_width/2, y=0, z=goal_height/2, length=goal_height, radius=0.05, collision_group = 'goalpost', mass=999)
+        right_post = sim.send_cylinder(x=-goal_width/2, y=0, z=goal_height/2, length=goal_height, radius=0.05, collision_group = 'goalpost', mass=999)
+        crossbar = sim.send_cylinder(x=0, y=0, z=goal_height, r1=1, r2=0, r3=0, length=goal_width, radius=0.05, collision_group = 'goalpost', mass=999)
         sim.send_fixed_joint(left_post, crossbar)
         sim.send_fixed_joint(right_post, crossbar)
         
