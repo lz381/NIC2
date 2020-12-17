4650849350a954c7c31f840ad6ac8c5a0dc77dd1
ShaneR <srichardson1928@hotmail.co.uk>
2020-12-17 12:16:14 +0000

    fixed goal post falling over, it started out in the ground, and added supports to be safe

--- environment.py
+++ environment.py
@@ -15,13 +15,19 @@ class ENVIRONMENT:
         # goalpost params
         goal_width = 5
         goal_height = 2
+        supportLength = 0.5
+        postRad = 0.05
         
         # goalpost object
-        left_post = sim.send_cylinder(x=goal_width/2, y=0, z=goal_height/2, length=goal_height, radius=0.05, collision_group = 'goalpost', mass=999)
-        right_post = sim.send_cylinder(x=-goal_width/2, y=0, z=goal_height/2, length=goal_height, radius=0.05, collision_group = 'goalpost', mass=999)
-        crossbar = sim.send_cylinder(x=0, y=0, z=goal_height, r1=1, r2=0, r3=0, length=goal_width, radius=0.05, collision_group = 'goalpost', mass=999)
+        left_post = sim.send_cylinder(x=goal_width/2, y=0, z=goal_height/2+postRad, length=goal_height, radius=postRad, collision_group = 'goalpost', mass=999)
+        right_post = sim.send_cylinder(x=-goal_width/2, y=0, z=goal_height/2+postRad, length=goal_height, radius=postRad, collision_group = 'goalpost', mass=999)
+        crossbar = sim.send_cylinder(x=0, y=0, z=goal_height+postRad, r1=1, r2=0, r3=0, length=goal_width, radius=postRad, collision_group = 'goalpost', mass=999)
+        leftSupport = sim.send_cylinder(x=goal_width/2, y=-supportLength/2, z=postRad, r1=0, r2=1, r3=0, length=supportLength, radius=postRad, collision_group='goalpost', mass=999)
+        rightSupport = sim.send_cylinder(x=-goal_width/2, y=-supportLength/2, z=postRad, r1=0, r2=1, r3=0, length=supportLength, radius=postRad, collision_group='goalpost', mass=999)
         sim.send_fixed_joint(left_post, crossbar)
         sim.send_fixed_joint(right_post, crossbar)
+        sim.send_fixed_joint(leftSupport, left_post)
+        sim.send_fixed_joint(rightSupport, right_post)
         
         
         # first shot at goal
