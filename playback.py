cef6c96aa8b187dfbf3f0b4e1aabc7dd211381e6
ShaneR <srichardson1928@hotmail.co.uk>
2020-12-22 10:48:10 +0000

    made it playback in all tested envs

--- playback.py
+++ playback.py
@@ -7,5 +7,24 @@ envs = ENVIRONMENTS()
 
 with open('RobotReplayGen_0.p', 'rb') as f:
     best = pickle.load(f)
-    best.Start_Evaluation(env=envs.envs[0], pb=False)
-    best.Compute_Fitness()
+    for i in range(c.numEnvs):
+        best.Start_Evaluation(env=envs.envs[i], pb=False)
+        best.Compute_Fitness()
+
+# with open('RobotReplayGen_0.p', 'rb') as f:
+#     best = pickle.load(f)
+#     for i in range(c.numEnvs):
+#         best.Start_Evaluation(env=envs.envs[i], pb=False)
+#         best.Compute_Fitness()
+    
+# with open('RobotReplayGen_0.p', 'rb') as f:
+#     best = pickle.load(f)
+#     for i in range(c.numEnvs):
+#         best.Start_Evaluation(env=envs.envs[i], pb=False)
+#         best.Compute_Fitness()
+
+# with open('RobotReplayGen_0.p', 'rb') as f:
+#     best = pickle.load(f)
+#     for i in range(c.numEnvs):
+#         best.Start_Evaluation(env=envs.envs[i], pb=False)
+#         best.Compute_Fitness()
