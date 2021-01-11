9024eae567afa2549edaca086c9cc4030bdef52b
Dex <74717491+dex426@users.noreply.github.com>
2021-01-11 17:04:47 +0000

    Update run_testbench.py

--- run_testbench.py
+++ run_testbench.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan  8 12:19:05 2021
-
-@author: leon
 
 """
+This module is a testbench used to check for overfitted models. 
+X number of randomly generated shots are created using the random seed from the constants module
+These randomly generated shots are more challenging than the ones the robot has previously trained on.
 
+Warning: if the testbench is run with instant replay enabled scores will be calculated in real time
+
+
+"""
+
+# import the packages
 import numpy as np
 import pickle
 import constants as c
@@ -19,7 +22,7 @@ class TESTS:
         
         self.envs = {}
         
-        # dict of 100 tests
+        # dictionary ID for each of the shots
         for e in range(0, numTests):
             self.envs[e] = TEST(e)
 
@@ -29,14 +32,14 @@ class TEST:
     def __init__(self, env_id):
         self.ID = env_id
 
-        # ball params
+        # ball parameters
         self.ball_radius = 0.16
         self.ball_z = self.ball_radius
         self.ball_psensor_id = 0
         
     def Send_To(self, sim):
         
-        # goalpost params
+        # goalpost parameters
         goal_width = 5
         goal_height = 2
         supportLength = 0.5
@@ -51,15 +54,17 @@ class TEST:
         rightSupport = sim.send_cylinder(x=-goal_width/2, y=-100, z=postRad, r1=0, r2=1, r3=0, length=supportLength, radius=postRad, collision_group='goalpost', mass=999)
         
         
+        # send to pyrosim
         sim.send_fixed_joint(left_post, crossbar)
         sim.send_fixed_joint(right_post, crossbar)
         sim.send_fixed_joint(leftSupport, left_post)
         sim.send_fixed_joint(rightSupport, right_post)
         
         
-        # left < as might want to add aireal or more challenging shots after a number of gens. Could be good to stop over fitting?
+        # Has been left as an absurdly large number for the inequality. 
+        # This could be used to differentiate between experiments that save all shots and have a very large score. 
         # E.g if a car saves 6/10 shots give it the ability to try 3 more complex shots. otherwise return 0 and dont start sim for extra shots. 
-        if self.ID < 1000:
+        if self.ID < 100000:
             y_speed = np.random.randint(-25,-10)
             x_speed = np.random.randint(-33,33)*y_speed/100
             ball = sim.send_sphere(x=0, y=6, z=self.ball_z, radius=self.ball_radius, collision_group = 'ball')
@@ -96,11 +101,12 @@ with open(filename, 'rb') as f:
         for i in range(numTests):
             best.fitness = 0 
             best.Start_Evaluation(env=envs.envs[i], pb=True)
+            
             #best.sim.wait_to_finish()
             best.Compute_Fitness(metric='best_keeper')
             print(best.fitness)
             total_score += best.fitness
             counter += 1
-        #print('Fitness of the best in this generation: ', best.fitness)
+            
         final_scores.append(total_score/counter)
     print(final_scores)
